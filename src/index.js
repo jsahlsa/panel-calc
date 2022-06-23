d1bb0f1afb7a2289ae25418a96d79a3acaa9f4e0
Joe Sahlstrom <joe.sahlstrom@gmail.com>
2022-06-23 18:04:46 +0000

    added number of panels to result

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const submitBtn = document.querySelector('input[type="submit"]');
 const input = document.querySelector('.input');
 const finalSize = document.querySelector('#panel-width');
+const totalPanels = document.querySelector('#number-of-panels');
 
 const calculatePanels = (e) => {
     e.preventDefault();
@@ -20,15 +21,16 @@ const calculatePanels = (e) => {
     const panelSize = widthWithGaps / numberOfPanels;
     // The rest is to make it look nice and validate
     let panelString = panelSize.toString();
+    // ensure result is limited to thousandths place
     let panelArray = panelString.split('');
-    console.log(panelArray);
-
     let newPanelArray =
         panelArray.length > 6 ? panelArray.slice(0, 6) : panelArray;
     let panel = newPanelArray.join('');
+    // test for valid input
     finalSize.textContent = !Number.isNaN(Number(panel))
         ? `${panel}"`
         : 'Please enter a valid number';
+        totalPanels.textContent = numberOfPanels;
 };
 
 submitBtn.addEventListener('click', calculatePanels);
